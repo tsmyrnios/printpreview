da7bc304a3f42deca40f8c7c065575c22fbdae80
Tadd Smyrnios <tadd.smyrnios@gmail.com>
2013-02-06 15:35:10 -0500

    ran JSLint. thanks Neil.

--- printpreview.js
+++ printpreview.js
@@ -1,47 +1,55 @@
-; (function($) {
+/*global jQuery */
+(function ($) {
+    "use strict";
     $.fn.extend({
-        savePrintElement: function() {
-            var target = $(this);
-            var printElement = target.next(".print-element");
-            printElement = printElement.attr("class") != undefined ? printElement : $("<span></span>").addClass("print-element").insertAfter(target);
+        savePrintElement: function () {
+            var target = $(this),
+                printElement = target.next(".print-element"),
+                selectedOptions = null,
+                selectedTextValues = [];
+
+            printElement = printElement.attr("class") !== undefined ? printElement : $("<span></span>").addClass("print-element").insertAfter(target);
+
             if (target.is("select")) {
-                var selectedOptions = target.children("option:selected");
-                var selectedTextValues = new Array();
-                selectedOptions.each(function(i, selectedOption) {
-                    selectedTextValues.push($(selectedOption).text());
-                });
+                /*jslint unparam: true*/
+                selectedOptions = target.children("option:selected")
+                    .each(function (i, selectedOption) {
+                        selectedTextValues.push($(selectedOption).text());
+                    });
+                /*jslint unparam: false*/
                 printElement.text(selectedTextValues.join(","));
-            }
-            else if (target.is("input[type=text]")) {
+            } else if (target.is("input[type=text]")) {
                 printElement.addClass("input-" + target.attr("type") + "-print").text(target.val());
-            }
-            else {
+            } else {
                 printElement.text(target.val());
             }
 
-            if (!target.is(":visible"))
+            if (!target.is(":visible")) {
                 printElement.hide();
+            }
         }
     });
 
-    $.setupPrintPreview = function() {
-        $("input[type=text],textarea,select").on("change", function() {
+    $.setupPrintPreview = function () {
+        $("input[type=text],textarea,select").on("change", function () {
             $(this).savePrintElement();
         });
 
-        $("input[type=text],textarea,select").each(function(i, element) {
+        /*jslint unparam: true*/
+        $("input[type=text],textarea,select").each(function (i, element) {
             var $element = $(element);
             $element.addClass("screen-element");
             $element.savePrintElement();
         });
 
-        $("input[type=text]").each(function(i, element) {
+        $("input[type=text]").each(function (i, element) {
             var $element = $(element);
             $element.addClass("input-" + $element.attr("type"));
         });
+        /*jslint unparam: false*/
     };
-})(jQuery);
 
-$(function() {
-	$.setupPrintPreview();
-});
+    $(function () {
+        $.setupPrintPreview();
+    });
+}(jQuery));
